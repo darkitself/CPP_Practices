c7d94929641168aba9b633b78e26576384c2c411
darkitself <evgeniymrlii@gmail.com>
2020-11-28 16:15:55 +0500

    Initialize

--- Main.cpp
+++ Main.cpp
@@ -9,5 +9,14 @@ int main()
 		maze.makeConnection(i, i + 1, i + 1, i + 1);
 	}
 	maze.printMaze();
+	cout << endl;
+
+	Maze maze2(5, 5);
+	for (int i = 0; i < maze.rows() - 1; i++)
+	{
+		maze2.makeConnection(i, i + 1, i, i);
+		maze2.makeConnection(i + 1, i + 1, i, i + 1);
+	}
+	maze2.printMaze();
 	return 0;
 }
